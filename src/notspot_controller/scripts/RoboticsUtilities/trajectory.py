571259548b2af3fe4b7821ca861ba2ee61942cf4
Prasham-Patel <prasham.patel@outlook.com>
2022-11-13 16:58:14 -0500

    first

---
+++ src/notspot_controller/scripts/RoboticsUtilities/trajectory.py
@@ -0,0 +1,52 @@
+import numpy as np
+from math import atan, sin, cos, pi
+from matplotlib import pyplot as plt
+import copy
+
+class trajectory():
+
+    def __init__(self, xi, xf, vi, vf, t0, tf):
+        self.xi  = xi
+        self.xf = xf
+        self.Ti = t0
+        self.Tf = tf
+        self.vi = vi
+        self.vf = vf
+
+        self.x_matrix = np.matrix([[xi], [vi], [xf], [vf]])
+        self.cubic_T_Matrix()
+        self.cubic_trajectory()
+
+
+    def cubic_T_Matrix(self):
+        self.T_matrix = np.matrix([[1, self.Ti, self.Ti ** 2, self.Ti ** 3],
+                                  [0, 1, 2 * self.Ti, 3 * self.Ti ** 2],
+                                  [1, self.Tf, self.Tf ** 2, self.Tf ** 3],
+                                  [0, 1, 2 * self.Tf, 3 * self.Tf ** 2]])
+        self.T_inv = np.linalg.inv(self.T_matrix)
+
+    def cubic_trajectory(self):
+        self.cubic_T_Matrix()
+        self.x_Co_ef = np.dot(self.T_inv, self.x_matrix)
+        
+
+    def at_time(self, T):
+        T_d1 = np.matrix([1, T, T**2, T**3])
+        T_d2 = np.matrix([0, 1, 2*T, 3*T**2])
+        x = np.dot(T_d1, self.x_Co_ef)
+        x_dot = np.dot(T_d2, self.x_Co_ef)
+        return x, x_dot
+
+if __name__ == '__main__':
+
+    primitive = trajectory(0, 1, 0, 0, 0, 1)
+    pos_x = []
+    pos_y = []
+    time = []
+    for i in range(100):
+        x, y = primitive.at_time(i/100)
+        pos_x.append(x)
+        pos_y.append(y)
+        time.append(i)
+    plt.scatter(time, pos_x)
+    plt.show()
